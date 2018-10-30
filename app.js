fbbed346aaf3626065f4935707d32e16605771b5
Ricardo Rodriguez <rr092293@gmail.com>
2018-10-30 10:47:57 -0700

    Users can see all reviews

--- app.js
+++ app.js
@@ -5,9 +5,19 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+let reviews = [
+    {title: "Great Review", movieTitle: "Ironman"},
+    {title: "Awesome Movie", movieTitle: "The Avengers"}
+]
+
+
+// app.get('/', (req, res) => {
+//     res.render('home', {msg: 'Handlebars are cool!'});
+// });
+
 app.get('/', (req, res) => {
-    res.render('home', {msg: 'Handlebars are cool!'});
-});
+    res.render('reviews-index', {reviews: reviews});
+})
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
