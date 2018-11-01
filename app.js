f39319d017598cb45719cde183086179a4233418
Ricardo Rodriguez <rr092293@gmail.com>
2018-11-01 10:30:31 -0700

    added view controller

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const app = express();
 
+
 mongoose.connect('mongodb://localhost/rotten-potatoes');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -18,78 +19,9 @@ const Review = mongoose.model('Review', {
     rating: Number
 });
 
-// let reviews = [
-//     {title: "Great Review", movieTitle: "Ironman"},
-//     {title: "Awesome Movie", movieTitle: "The Avengers"},
-//     {title: "Awesome Movie", movieTitle: "Guardians of the Galaxy"}
-// ]
+const reviews = require('./controllers/reviews')(app, Review);
 
 
-// app.get('/', (req, res) => {
-//     res.render('home', {msg: 'Handlebars are cool!'});
-// });
-
-
-// Index
-app.get('/', (req, res) => {
-    Review.find()
-        .then(reviews => {
-            res.render('reviews-index', {reviews: reviews});
-        })
-        .catch(err => {
-            console.log(err);
-        })
-})
-
-// New
-app.get('/reviews/new', (req, res) => {
-    res.render('reviews-new', {});
-})
-
-// Show
-app.get('/reviews/:id', (req, res) => {
-    Review.findById(req.params.id).then((review) => {
-        res.render('reviews-show', {review: review});
-    }).catch((err) => {
-        console.log(err.message);
-    })
-})
-
-// Create
-app.post('/reviews', (req, res) => {
-    Review.create(req.body).then(review => {
-        console.log(review);
-        res.redirect('/reviews/'+ review._id);
-    }).catch(err => {
-        console.log(err.message);
-    })
-})
-
-// Edit
-app.get('/reviews/:id/edit', (req, res) => {
-    Review.findById(req.params.id).then((review) => {
-        res.render('reviews-edit', {review: review});
-    })
-})
-
-// Update
-app.put('/reviews/:id', (req, res) => {
-    Review.findByIdAndUpdate(req.params.id, req.body).then(review => {
-        res.redirect('/reviews/'+ review._id);
-    }).catch((err) => {
-        console.log(err.message);
-    })
-})
-
-// DELETE
-app.delete('/reviews/:id', (req, res) => {
-    console.log("DELETE review");
-    Review.findByIdAndRemove(req.params.id).then((review) => {
-        res.redirect('/');
-    }).catch((err) => {
-        console.log(err.message);
-    })
-})
 
 
 
---
+++ controllers/reviews.js
@@ -0,0 +1,62 @@
+// reviews.js
+
+module.exports = function(app, Review) {
+    //Index
+    app.get('/', (req, res) => {
+        Review.find().then(reviews => {
+            res.render('reviews-index', {reviews: reviews});
+        }).catch(err => {
+            console.log(err.message);
+        })
+    })
+
+    //New
+    app.get('/reviews/new', (req, res) => {
+        res.render('reviews-new', {});
+    })
+    // Show
+    app.get('/reviews/:id', (req, res) => {
+        Review.findById(req.params.id).then((review) => {
+            res.render('reviews-show', {review: review});
+        }).catch((err) => {
+            console.log(err.message);
+        })
+    })
+
+    // Create
+    app.post('/reviews', (req, res) => {
+        Review.create(req.body).then(review => {
+            console.log(review);
+            res.redirect('/reviews/'+ review._id);
+        }).catch(err => {
+            console.log(err.message);
+        })
+    })
+
+    // Edit
+    app.get('/reviews/:id/edit', (req, res) => {
+        Review.findById(req.params.id).then((review) => {
+            res.render('reviews-edit', {review: review});
+        })
+    })
+
+    // Update
+    app.put('/reviews/:id', (req, res) => {
+        Review.findByIdAndUpdate(req.params.id, req.body).then(review => {
+            res.redirect('/reviews/'+ review._id);
+        }).catch((err) => {
+            console.log(err.message);
+        })
+    })
+
+    // DELETE
+    app.delete('/reviews/:id', (req, res) => {
+        console.log("DELETE review");
+        Review.findByIdAndRemove(req.params.id).then((review) => {
+            res.redirect('/');
+        }).catch((err) => {
+            console.log(err.message);
+        })
+    })
+
+}
