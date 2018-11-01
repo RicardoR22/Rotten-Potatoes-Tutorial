67207202b237cf416a9a32aef56e62028f58b71c
Ricardo Rodriguez <rr092293@gmail.com>
2018-10-31 22:35:38 -0700

    Reviews can now be deleted

--- app.js
+++ app.js
@@ -75,9 +75,19 @@ app.get('/reviews/:id/edit', (req, res) => {
 // Update
 app.put('/reviews/:id', (req, res) => {
     Review.findByIdAndUpdate(req.params.id, req.body).then(review => {
-        res.redirect('/reviews/'+ review._id)
+        res.redirect('/reviews/'+ review._id);
     }).catch((err) => {
-        console.log(err.message)
+        console.log(err.message);
+    })
+})
+
+// DELETE
+app.delete('/reviews/:id', (req, res) => {
+    console.log("DELETE review");
+    Review.findByIdAndRemove(req.params.id).then((review) => {
+        res.redirect('/');
+    }).catch((err) => {
+        console.log(err.message);
     })
 })
 
